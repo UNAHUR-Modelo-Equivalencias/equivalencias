5ac905fa505a97204d42cd71af3fac315b384948
lmcasella <lautaromcasella@gmail.com>
2022-07-21 22:06:28 -0300

    uri

--- equivalencias-front-end/src/config/config.js
+++ equivalencias-front-end/src/config/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    apiUrl: 'postgres://ptakwrapckjkne:22f93e633f0d18873cf95bc6375aee7778107581cd2fcacbc3515f4ccedb0b69@ec2-54-161-255-125.compute-1.amazonaws.com:5432/dbktvmpppt8nt7'
+    apiUrl: 'http://postgres://ptakwrapckjkne:22f93e633f0d18873cf95bc6375aee7778107581cd2fcacbc3515f4ccedb0b69@ec2-54-161-255-125.compute-1.amazonaws.com:5432/dbktvmpppt8nt7'
 };
