be3b45a5fe78931af0ad1ac03e0b3f1fcd3c3bbc
Emanuelr9 <emanuelr9@yahoo.com.ar>
2022-07-22 19:51:32 -0300

    make it better

--- equivalencias-back-end/lib/config/config.js
+++ equivalencias-back-end/lib/config/config.js
@@ -4,7 +4,7 @@ const parse = require('pg-connection-string').parse;
 const dotenv = require('dotenv');
 
 function getEnvironment() {
-  return 'production';
+  return process.env.NODE_ENV || 'development';
 }
 
 function initializeEnv() {
@@ -14,7 +14,7 @@ function initializeEnv() {
 }
 
 function parseHerokuUrlIfPresent() {
-  const url = 'postgres://ptakwrapckjkne:22f93e633f0d18873cf95bc6375aee7778107581cd2fcacbc3515f4ccedb0b69@ec2-54-161-255-125.compute-1.amazonaws.com:5432/dbktvmpppt8nt7'
+  const url = process.env.DATABASE_URL;
 
   if (url === undefined) {
     return {};
@@ -49,11 +49,11 @@ function normalizePort(val) {
 function initializeConfig() {
   const environment = getEnvironment();
   let dbConfig = {
-    username: 'ptakwrapckjkne',
-    password: '22f93e633f0d18873cf95bc6375aee7778107581cd2fcacbc3515f4ccedb0b69',
-    database: 'dbktvmpppt8nt7',
-    host: 'ec2-54-161-255-125.compute-1.amazonaws.com',
-    port: '5432',
+    username: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    host: process.env.SQL_HOST || 'localhost',
+    port: process.env.SQL_PORT || '5432',
     dialect: 'postgres',
     logging: debug('sequelize'),
   };
